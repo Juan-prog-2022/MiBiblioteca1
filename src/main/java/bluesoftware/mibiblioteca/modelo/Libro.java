d0015cc1e430e4fdb6fbe29f10c7283123869493
Juan Carlos Quiroz <quirozjuan497@gmail.com>
2024-04-15 20:08:39 -0300

    Creacion de entidad Libro y Base de Datos

---
+++ src/main/java/bluesoftware/mibiblioteca/modelo/Libro.java
@@ -0,0 +1,25 @@
+package bluesoftware.mibiblioteca.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id_Libro;
+    String nombre_Libro;
+    String autor;
+    Double precio;
+    Integer existencias;
+}
